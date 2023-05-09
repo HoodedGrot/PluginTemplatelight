83d8ba07f57593d999fef86e0435c0828ee40d1a
HoodedGrot <132997670+HoodedGrot@users.noreply.github.com>
2023-05-09 10:30:52 +0100

    Initial commit

---
+++ src/main/java/org/mineacademy/template/HoodedLearningplugin.java
@@ -0,0 +1,45 @@
+package org.mineacademy.template;
+
+import org.bukkit.entity.EntityType;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.player.PlayerInteractEntityEvent;
+import org.mineacademy.fo.plugin.SimplePlugin;
+
+/**
+ * PluginTemplate is a simple template you can use every time you make
+ * a new plugin. This will save you time because you no longer have to
+ * recreate the same skeleton and features each time.
+ * <p>
+ * It uses Foundation for fast and efficient development process.
+ */
+public final class HoodedLearningplugin extends SimplePlugin {
+
+	/**
+	 * Automatically perform login ONCE when the plugin starts.
+	 */
+	@Override
+	protected void onPluginStart() {
+		System.out.println(1);
+		System.out.println("knock, knock!" + (1 + 7) + " crystal wants to stab hooded");
+
+	}
+// woooooo comment
+
+	/* woah is this a comment */
+	/* ------------------------------------------------------------------------------- */
+	/* Events */
+	/* ------------------------------------------------------------------------------- */
+
+	/**
+	 * An example event that checks if the right clicked entity is a cow, and makes an explosion.
+	 * You can write your events to your main class without having to register a listener.
+	 *
+	 * @param event
+	 */
+	@EventHandler
+	public void onRightClick(PlayerInteractEntityEvent event) {
+		if (event.getRightClicked().getType() == EntityType.PIG)
+			event.getRightClicked().getWorld().createExplosion(event.getRightClicked().getLocation(), 5);
+
+	}
+}
